ac43bf40f65461b255e3ea9aeb3eaa27838353aa
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-02-05 17:02:34 +0530

    Connction String Updated

---
+++ Happiness-Index/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Happiness_Index
+{
+    public class DBConnection
+    {
+        public static string ConnectionString = "Data Source=DESKTOP-DCCUTP6\\SQLEXPRESS;Initial Catalog=MAS-KREEDA-Synergy-HI-App;Integrated Security=true";
+        //User ID = YourUsername; Password=YourPassword;
+    }
+}
